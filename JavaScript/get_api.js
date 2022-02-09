f7da025faf27b3f4f37c575233d13b61180b4d39
mohammad-moussalli <mohammadmoussalli@gmail.com>
2022-02-09 12:53:53 +0200

    Add buttons and APIs

---
+++ JavaScript/get_api.js
@@ -0,0 +1,6 @@
+let buttons = document.getElementsByClassName("buttons");
+
+let api1 = "http://localhost/API-Assignment/PHP/api1.php?";
+let api2 = "http://localhost/API-Assignment/PHP/api2.php?";
+let api3 = "http://localhost/API-Assignment/PHP/api3.php?";
+let api4 = "http://localhost/API-Assignment/PHP/api4.php?";
