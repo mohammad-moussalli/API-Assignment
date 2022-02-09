5400754ad0d347515a8007a6e9519e6544be7af0
mohammad-moussalli <mohammadmoussalli@gmail.com>
2022-02-09 11:57:11 +0200

    Build the fourth api to vhech validity of password

---
+++ PHP/api4.php
@@ -0,0 +1,16 @@
+<?php
+
+$password = $_GET["password"];
+$hashed_password = hash('sha256', $password);
+$valid_password = FALSE;
+
+if (preg_match('/[A-Za-z]/', $password) && preg_match('/[0-9]/', $password) && strlen($password) > 8){
+    $valid_password = TRUE;
+}
+
+$array =["password" => $hashed_password,
+         "password_validity" => $valid_password];
+
+echo json_encode($array);
+
+?>
