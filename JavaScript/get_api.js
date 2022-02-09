9ba78de4f2929a3334585b3c238394cbb2fd91e0
mohammad-moussalli <mohammadmoussalli@gmail.com>
2022-02-09 13:10:53 +0200

    Get first API

--- JavaScript/get_api.js
+++ JavaScript/get_api.js
@@ -1,6 +1,22 @@
 let buttons = document.getElementsByClassName("buttons");
 
-let api1 = "http://localhost/API-Assignment/PHP/api1.php?";
 let api2 = "http://localhost/API-Assignment/PHP/api2.php?";
 let api3 = "http://localhost/API-Assignment/PHP/api3.php?";
 let api4 = "http://localhost/API-Assignment/PHP/api4.php?";
+
+buttons[0].addEventListener("click", function(){
+    let x = document.getElementById("x").value;
+    let y = document.getElementById("y").value;
+    let api1 = "http://localhost/API-Assignment/PHP/api1.php?";
+    document.getElementById("x_value").textContent = "X is equal to: ";
+    document.getElementById("y_value").textContent = "Y is equal to: ";
+    api1 += "x=" + x + "&y="  + y;
+    getAPI1(api1);
+});
+
+async function getAPI1(url){
+    const response = await fetch(url);
+    const json_object = await response.json();
+    document.getElementById("x_value").textContent += json_object.x;
+    document.getElementById("y_value").textContent += json_object.y;
+}
